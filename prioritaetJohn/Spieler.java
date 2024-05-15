2e610d463a2f84738efb2c625566165bd7bbc3f5
ian.warnebie <ian.warnebier@hebk.de>
2024-05-15 11:14:55 +0200

    [CHANGED] Spieler.java

--- prioritaetJohn/Spieler.java
+++ prioritaetJohn/Spieler.java
@@ -7,6 +7,14 @@ public class Spieler {
         name = pName;
     }
     public void entscheidungTreffen(int pEntscheidung){
+        if(pEntscheidung == 1){
 
+        } else if (pEntscheidung == 2) {
+
+        } else if (pEntscheidung == 3) {
+
+        } else {
+            System.out.println("Die Entscheidung gibt es nicht bist du bescheuert, das Spiel ist nicht mal schwer!!!");
+        }
     }
 }
