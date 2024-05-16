5ec2e21a98754391afcd7207138c25716ce15a7f
jost.hoetker <jost.hoetker@hebk.de>
2024-05-16 12:51:40 +0200

    [CHANGED] Attributes to private [ADDED] get- and set-method for the attributes [ADDED] Constructor

--- prioritaetJohn/NPC.java
+++ prioritaetJohn/NPC.java
@@ -1,11 +1,34 @@
 package prioritaetJohn;
 
 public class NPC {
-    String name;
-    boolean gerettet;
-    boolean lebend;
+    private String name;
+    private boolean gerettet;
+    private boolean lebend;
 
-    public void gerettetWerden() {
+    public NPC(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isGerettet() {
+        return gerettet;
+    }
+    public void setGerettet() {
         gerettet = true;
+    }
+
+    public void setLebend(boolean lebend) {
+        this.lebend = lebend;
+    }
+
+    public boolean isLebend() {
+        return lebend;
     }
 }
