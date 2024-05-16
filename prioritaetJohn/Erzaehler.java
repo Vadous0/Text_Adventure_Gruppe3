6e73dd11034c5c97e297ce699b2f22724792dba7
ian.warnebie <ian.warnebier@hebk.de>
2024-05-16 14:35:00 +0200

    [CHANGED] Erzaehler.java

---
+++ prioritaetJohn/Erzaehler.java
@@ -0,0 +1,11 @@
+package prioritaetJohn;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Erzaehler {
+    Scanner scanDecision = new Scanner(System.in);
+    public void entscheidungTreffen( String scanDecision ){
+
+    }
+}
